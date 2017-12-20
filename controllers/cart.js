43ebddde779fc1ce4066b593c9d38bac2a1c85d9
zce <w@zce.me>
2017-12-20 18:03:35 +0800

    :tada: cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,1 +1,25 @@
-// exports
+const createError = require('http-errors')
+
+const { Goods, UserCart } = require('../models')
+
+exports.index = (req, res, next) => {
+
+}
+
+exports.add = (req, res, next) => {
+  if (isNaN(req.query.id)) return next(createError(400, '缺少必要参数'))
+
+  Goods.findOne({ where: { goods_id: req.query.id } })
+    .then(goods => {
+      if (!goods) return next(createError(404, '没有这个商品'))
+
+      res.locals.goods = goods
+
+      const cartInfo = req.cookies['cart_info'] || []
+      cartInfo.push({ goods_id: goods.goods_id })
+      res.cookie('cart_info', cartInfo, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) })
+
+      res.render('cart/add')
+    })
+    .catch(next)
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const router = new Router()
 
-router.get('/', (req, res, next) => {
-  res.send('respond with cart page')
-})
+const cartController = require('../controllers/cart')
+
+router.get('/', cartController.index)
+router.get('/add', cartController.add)
 
 module.exports = router
